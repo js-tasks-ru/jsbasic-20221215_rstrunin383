1f524485698ac0be8d25f630c172d9697440792e
rstrunin <rstrunin383@gmail.com>
2022-12-28 13:12:37 +0300

    Исправлена задача 1.5 Добавлены задачи 2.1-3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,24 @@
 let calculator = {
-  // ваш код
+  read(a, b) {
+    this.a = a;
+    this.b = b;
+  },
+
+  sum() {
+    if (!isNaN(Number(this.a)) && !isNaN(Number(this.b))) {
+      return this.a + this.b;
+    } else {
+      console.log('Invalid input');
+    }
+  },
+
+  mul() {
+    if (!isNaN(Number(this.a)) && !isNaN(Number(this.b))) {
+      return this.a * this.b;
+    } else {
+      console.log('Invalid input');
+    }
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
