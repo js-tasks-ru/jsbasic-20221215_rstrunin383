7665b3b0a5a9c38a7f843fe5ca05b6eae6016113
rstrunin <rstrunin383@gmail.com>
2023-01-13 15:05:49 +0300

    Добавлена задача 5.1

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,7 @@
 function hideSelf() {
-  // ваш код...
+  let element = document.querySelector('.hide-self-button');
+  
+  element.addEventListener('click', () => {
+    element.hidden = 'true';
+  });
 }
